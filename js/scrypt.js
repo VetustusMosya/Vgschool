1e44e48561b69c11d1af087581bb4df330a17225
Moiseev <moiseevsenya2002@gmail.com>
2022-07-20 20:18:59 +0300

    header inaf

--- js/scrypt.js
+++ js/scrypt.js
@@ -3,9 +3,10 @@ function menuOnClick() {
   document.getElementById("menu-bar").classList.toggle("change");
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
+  document.querySelector(".header__logo").classList.toggle("header__logo--net");
 }
 // КнопОчка
 document.querySelector(".tariff__button").onclick = function () {
-  document.querySelector(".button__float").style.opacity = "0";
+  document.querySelector(".button__float").style.opacity = 0;
   document.querySelector(".button__float").style.top = "-45px";
 };
